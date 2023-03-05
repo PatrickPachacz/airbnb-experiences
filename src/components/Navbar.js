9e6bfe7c2d6db9bb393ee3915e9d0166a0d336b2
Patrick <patrickpachacz0@gmail.com>
2023-03-04 19:57:39 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Navbar(){
+    return (
+        
+            <nav>
+                <img src="./images/airbnb.png" className="nav--logo" alt="logo" lazy="true" />
+                
+            </nav>
+        
+    )
+}
